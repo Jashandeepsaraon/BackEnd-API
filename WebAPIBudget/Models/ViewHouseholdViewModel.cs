d536a102bc9d570ede95fa489f41afeda3984e63
Jashan Saraon <Jashanp.k99@gmail.com>
2019-06-14 08:28:34 -0500

    Add project files.

---
+++ WebAPIBudget/Models/ViewHouseholdViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIBudget.Models
+{
+    public class ViewHouseholdViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumberOfUsers { get; set; }
+        public bool IsOwner { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+    }
+}
